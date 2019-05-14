ba453bc9023c15c38495a21e8128ffa3ca935716
shay keinan <crazypixel@gmail.com>
2019-05-14 08:40:26 +0300

    init

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import React, {Component} from 'react';
-import {Provider} from 'react-redux';
-import store from './redux/store';
-import Counter from './components/Counter';
 
 class App extends Component {
 	render() {
 		return (
-			<Provider store={store}>
-				<Counter />
-			</Provider>
+			<div>hello React and Redux course</div>
 		);
 	}
 }
--- src/redux/actions/counter.actions.js
+++
@@ -1,9 +0,0 @@
-import * as AT from '../action.types';
-
-export const increaseCounter = () => ({
-	type: AT.ADD
-});
-
-export const decreaseCounter = () => ({
-	type: AT.SUB
-});
